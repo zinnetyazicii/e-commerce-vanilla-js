5926243c13f24a112c1055b0bae51491e99b2435
zinnetyazicii <zinnetyazicii@gmail.com>
2022-08-31 16:22:38 +0300

    js

--- app.js
+++ app.js
@@ -1,4 +1,87 @@
+let bookList = [],
+  basketList = [];
+
+
+
 const toggleModal = () => {
-  const basketModalEl = document.querySelector(".basket_modal");
+  const basketModalEl = document.querySelector(".basket__modal");
   basketModalEl.classList.toggle("active");
 };
+
+const getBooks = () => {
+  fetch("./products.json")
+    .then((res) => res.json())
+    .then((books) => (bookList = books));
+};
+
+getBooks();
+
+const createBookStars = (starRate) => {
+  let starRateHtml = "";
+  for (let i = 1; i <= 5; i++) {
+    if (Math.round(starRate) >= i)
+      starRateHtml += `<i class="bi bi-star-fill active"></i>`;
+    else starRateHtml += `<i class="bi bi-star-fill"></i>`;
+  }
+
+  return starRateHtml;
+};
+
+const createBookItemsHtml = () => {
+  const bookListEl = document.querySelector(".book__list");
+  let bookListHtml = "";
+  bookList.forEach((book, index) => {
+    bookListHtml += `<div class="col-5 ${index % 2 == 0 && "offset-2"} my-5">
+    <div class="row book__card">
+      <div class="col-6">
+        <img
+          class="img-fluid shadow"
+          src="${book.imgSource}"
+          width="258"
+          height="400"
+        />
+      </div>
+      <div class="col-6 d-flex flex-column justify-content-between">
+        <div class="book__detail">
+          <span class="fos gray fs-5">${book.author}</span><br />
+          <span class="fs-4 fw-bold">${book.name}</span><br />
+          <span class="book__star-rate">
+            ${createBookStars(book.starRate)}
+            <span class="gray">${book.reviewCount} reviews</span>
+          </span>
+        </div>
+        <p class="book__description fos gray">
+          ${book.description}
+        </p>
+        <div>
+          <span class="black fw-bold fs-4 me-2">${book.price}₺</span>
+          ${
+            book.oldPrice
+              ? `<span class="fs-4 fw-bold old__price">${book.oldPrice}</span>`
+              : ""
+          }
+        </div>
+        <button class="btn__purple" onclick="addBookToBasket(${
+          book.id
+        })">ADD BASKET</button>
+      </div>
+    </div>
+  </div>`;
+  });
+
+  bookListEl.innerHTML = bookListHtml;
+};
+
+
+
+
+
+
+
+
+
+
+setTimeout(() => {
+  createBookItemsHtml();
+  createBookTypesHtml();
+}, 100);
